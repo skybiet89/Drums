f09a4f9de5d00bd4f83bb6b503e2b43e0b318c9c
Sumit k Yadav <skybiet89@gmail.com>
2020-08-31 13:14:30 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,84 @@
+//click
+
+document.querySelectorAll("button")[0].addEventListener("click", handleClick);
+document.querySelectorAll("button")[1].addEventListener("click", handleClick);
+document.querySelectorAll("button")[2].addEventListener("click", handleClick);
+document.querySelectorAll("button")[3].addEventListener("click", handleClick);
+document.querySelectorAll("button")[4].addEventListener("click", handleClick);
+document.querySelectorAll("button")[5].addEventListener("click", handleClick);
+document.querySelectorAll("button")[6].addEventListener("click", handleClick);
+
+function handleClick() {
+
+    var buttonInnerHTML = this.innerHTML;
+    buttonAnimation(buttonInnerHTML);
+    makeSound(buttonInnerHTML);
+
+}
+
+
+document.addEventListener("keydown", function(event) {
+    makeSound(event.key);
+    buttonAnimation(event.key)
+
+
+});
+
+
+function makeSound(key) {
+    switch (key) {
+        case "w":
+            var audio = new Audio('sounds/tom-1.mp3');
+            audio.play();
+
+            break;
+        case "a":
+            var audio = new Audio('sounds/tom-2.mp3');
+            audio.play();
+
+            break;
+        case "s":
+            var audio = new Audio('sounds/tom-3.mp3');
+            audio.play();
+
+            break;
+        case "d":
+            var audio = new Audio('sounds/tom-4.mp3');
+            audio.play();
+
+            break;
+        case "j":
+            var audio = new Audio('sounds/snare.mp3');
+            audio.play();
+
+            break;
+        case "k":
+            var audio = new Audio('sounds/kick-bass.mp3');
+            audio.play();
+
+            break;
+        case "l":
+            var audio = new Audio('sounds/crash.mp3');
+            audio.play();
+
+            break;
+
+        default:
+            alert("Sumit is Great.");
+            break;
+    }
+}
+
+
+function buttonAnimation(currentKey) {
+    var activeButton = document.querySelector("." + currentKey);
+
+    activeButton.classList.add("pressed");
+
+    setTimeout(function() {
+        activeButton.classList.remove("pressed", 100);
+    });
+
+
+
+}
